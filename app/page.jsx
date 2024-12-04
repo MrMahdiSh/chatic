c114cf938454ed3a15bc76068a703d55712cc09b
MrMahdiSh <mahdishoorabi@gmail.com>
2024-12-04 16:30:53 +0330

    index

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,17 @@ import firebaseApp from "utils/firebase/firebase";
 import useFcmToken from "utils/firebase/hooks/useFcmToken";
 import Header from "@/components/ui/header";
 import HeroSection from "@/components/ui/HeroSection";
-import { FaCalendar, FaCheck } from "react-icons/fa";
+import {
+  FaCalendar,
+  FaCheck,
+  FaPhone,
+  FaEnvelope,
+  FaMapMarkerAlt,
+  FaTelegram,
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa";
 import Button from "@/components/ui/button";
 
 export default function Page() {
@@ -212,11 +222,32 @@ export default function Page() {
           <div className="w-1/2">
             <div
               style={{ boxShadow: "rgb(0 0 0 / 20%) 0px 0px 30px" }}
-              className="w-full h-[500px] rounded-3xl flex flex-col justify-around items-center"
+              className="w-full h-[500px] rounded-3xl flex flex-col justify-around items-center p-10"
             >
               {/* inputs here */}
-              
+              <input
+                type="text"
+                placeholder="نام و نام خانوادگی"
+                className="bg-[#ECECEC] rounded-lg p-2 w-full h-16"
+              />
+              <input
+                type="text"
+                placeholder="شماره"
+                className="bg-[#ECECEC] rounded-lg p-2 w-full h-16"
+              />
+
+              <textarea
+                placeholder="متن پیام"
+                className="bg-[#ECECEC] rounded-lg p-2 text-right align-top w-full h-32 max-h-32 min-h-32"
+              />
+
+              <button className="bg-[#02292B] w-full h-[70px] text-white rounded-lg">
+                انتخاب
+              </button>
             </div>
+          </div>
+          <div className="w-1/2">
+            <img src="/images/guyWorking.png" alt="guyWorking" />
           </div>
         </div>
       </div>
@@ -224,21 +255,112 @@ export default function Page() {
   };
 
   const Footer = () => {
+    const socialMedias = [
+      {
+        icon: <FaTelegram size="30px" color="#4747C2" />,
+      },
+      {
+        icon: <FaFacebook size="30px" color="#4747C2" />,
+      },
+      {
+        icon: <FaLinkedin size="30px" color="#4747C2" />,
+      },
+      {
+        icon: <FaTwitter size="30px" color="#4747C2" />,
+      },
+    ];
     return (
       <div className="w-full mt-[10rem]">
-        <div></div>
+        <div className="w-full h-[120px] bg-[#F2F2F2] relative">
+          <div className="absolute h-full left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <img src="/images/logo/icon.png" width={100} alt="icon" />
+            <p className="text-center">چتیک</p>
+          </div>
+        </div>
+        <div className="w-full bg-[#F9F9F9] relative">
+          <div className="w-full flex flex-row-reverse justify-around p-10">
+            {/* Left Column for Licenses */}
+            <div className="w-1/4 flex flex-col gap-5">
+              <h1 className="text-center">نماد های اعتماد</h1>
+              <div className="flex flex-row gap-5">
+                <div className="bg-white w-1/2 h-[100px] rounded-lg"></div>
+                <div className="bg-white w-1/2 h-[100px] rounded-lg"></div>
+              </div>
+            </div>
+
+            {/* Gap */}
+            <div className="w-1/12"></div>
+
+            {/* Two Columns */}
+            <div className="w-2/4 flex flex-row gap-20">
+              {/* First Section */}
+              <div>
+                <h3 className="text-lg font-bold mb-4">
+                  همه روزه 24 ساعته همراه شما هستیم
+                </h3>
+                <ul className="flex flex-col gap-2">
+                  <li className="flex items-center gap-5">
+                    <FaPhone className="text-black mr-2" />
+                    021123456
+                  </li>
+                  <li className="flex items-center gap-5">
+                    <FaEnvelope className="text-black mr-2" />
+                    info@chatic.com
+                  </li>
+                  <li className="flex items-center gap-5">
+                    <FaMapMarkerAlt className="text-black mr-2" />
+                    تهران تهران
+                  </li>
+                </ul>
+              </div>
+
+              {/* Second Section */}
+              <div>
+                <h3 className="text-lg font-bold mb-4">دسترسی سریع</h3>
+                <ul className="flex flex-col gap-2">
+                  <li>امکانات</li>
+                  <li>قیمت</li>
+                  <li>مستندات</li>
+                  <li>راهنما</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/* social media place */}
+          <div className="w-full h-[50px]">
+            <div className="h-full mx-auto p-5 flex flex-row justify-center items-center gap-5">
+              {socialMedias.map((item, index) => (
+                <div
+                  style={{ boxShadow: "rgb(0 0 0 / 20%) 0px 0px 30px" }}
+                  className="w-[60px] h-[60px] flex justify-center items-center bg-white rounded-lg"
+                  key={index}
+                >
+                  {item.icon}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full bg-[#F2F2F2] h-10 relative flex justify-center items-center mt-10">
+          <p className="text-center">
+            تمامی حقوق مادی معنوی برای چتیک محفوظ است
+          </p>
+        </div>
       </div>
     );
   };
 
   return (
-    <div className="px-16">
-      <Header />
-      <HeroSection />
-      <AIPart />
-      <Features />
-      <PricingTable />
-      <CallUs />
+    <div>
+      <div className="px-16">
+        <Header />
+        <HeroSection />
+        <AIPart />
+        <Features />
+        <PricingTable />
+        <CallUs />
+      </div>
       <Footer />
     </div>
   );
